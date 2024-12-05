a4c21d59efd0d25e6a0bbcf0e0f81aedb699ebad
irsals07 <145377188+irsals07@users.noreply.github.com>
2024-12-05 08:55:53 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+
+
+class SecondFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button: Button = view.findViewById(R.id.button)
+        button.setOnClickListener {
+            val navController: NavController = view.findNavController()
+            navController.navigate(R.id.action_firstFragment_to_fragment_third)
+        }
+    }
+}
