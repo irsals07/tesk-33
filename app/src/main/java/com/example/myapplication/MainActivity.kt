6b4ecd4d5311692ea1bb8924466edeaea5f2d26d
BT_1N3_10 <jdt@>
2024-12-05 09:16:49 -0500

    task-34

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,13 +3,38 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfig: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val appBar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(appBar)
+        val navHostFragment: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val navGraph: NavGraph = navController.graph // get the navigation graph associated with the controller
+        val appBarConfig: AppBarConfiguration = AppBarConfiguration(navGraph) // create an app configuration based on the nav graph
+        setupActionBarWithNavController(navController, appBarConfig)
+
 
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val success: Boolean = navController.navigateUp(appBarConfig)
+        return success || super.onSupportNavigateUp()
+    }
+
+
 }
